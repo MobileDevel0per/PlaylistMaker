2cb32f1dddb4fd5d63fd651a36e8c0cc2cd66ed4
MobileDevel0per <mobiledevel0perwork@gmail.com>
2025-06-02 02:05:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication0206/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication0206
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.firstbutton)
+        val button2 = findViewById<Button>(R.id.secondbutton)
+        val button3 = findViewById<Button>(R.id.thirdbutton)
+
+
+        button1.setOnClickListener {
+            val displayIntent = Intent(this, Search::class.java)
+            startActivity(displayIntent)
+        }
+
+        button2.setOnClickListener {
+            val displayIntent = Intent(this, Mediateka::class.java)
+            startActivity(displayIntent)
+        }
+
+        button3.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
